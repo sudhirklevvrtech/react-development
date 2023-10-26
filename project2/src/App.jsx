a04bd03071471685ce489045d637b963ea2a5744
sudhir <sudhir.kumar@frenzopay.com>
2023-10-26 12:11:24 +0530

    responsive navbar in react

---
+++ project2/src/App.jsx
@@ -0,0 +1,97 @@
+import React, { useState } from 'react';
+import './App.css';
+import {FaBars} from 'react-icons/fa'
+import {AiOutlineClose} from 'react-icons/ai';
+import {BiSolidRightArrow} from 'react-icons/bi';
+
+const App = () => {
+  
+  const [openMenu, setOpenMenu] = useState(false);
+  const [dropdown, setDropdown] = useState(false);
+
+  const toggleMenu =()=>{
+    setOpenMenu(!openMenu);
+  }
+
+  const dropdownMenu = ()=>{
+    setDropdown(!dropdown);
+  }
+
+  return (
+    <div>
+    {/* desktop header */}
+      <header className="header">
+        <div className="container">
+         <nav className="navbar">
+         <a href="#" className="brand">Logo</a>
+          <ul className="navigation-nav">
+            <li className="navItem">
+              <a href="#" className="navLink">Home</a>
+            </li>
+            <li className="navItem">
+              <a href="#" className="navLink">About</a>
+            </li>
+            <li className="navItem">
+              <a href="#" className="navLink dropDown">
+              Services
+
+              <ul className="dropdownMenu">
+                <li className='navitem'><a href="#">Web Development</a></li>
+                <li className='navitem'><a href="#">App Development</a></li>
+                <li className='navitem'><a href="#">Digital Marketing</a></li>
+                <li className='navitem'><a href="#">Graphic Designing</a></li>
+
+              </ul>              
+              </a>
+
+            </li>
+            <li className="navItem">
+              <a href="#" className="navLink">Blog</a>
+            </li>
+            <li className="navItem">
+              <a href="#" className="navLink">Contact</a>
+            </li>
+          </ul>
+         </nav>
+        </div>
+      </header>
+
+      {/* desktop header end */}
+      {/* mobile header start */}
+      <div className="mobile-menu">
+        <div className="container">
+          <nav className="mobileNav">
+            <a href="#" className="brand">Brand</a>
+            <button onClick={toggleMenu} className='toggleMenu'>
+             {
+              openMenu ? <AiOutlineClose />  : <FaBars />
+             }
+            </button>
+            <div className={`navigation ${openMenu ? 'active' : ''}`}>
+              <ul className="navbar-nav">
+                <li><a href="#">Home</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#" onClick={dropdownMenu} className='responsive-dropdown'>
+                Service <BiSolidRightArrow />
+                <ul className={`responsive-dropdown-menu ${dropdown ? 'active' : ''}`}>
+                  <li><a href="#">Web Development</a></li>
+                  <li><a href="#">App Development</a></li>
+                  <li><a href="#">Digital Marketing</a></li>
+                  <li><a href="#">Graphic Designing</a></li>
+
+                </ul>
+                </a>
+                </li>
+                <li><a href="#">Blog</a></li>
+                <li><a href="#">Contact</a></li>
+
+              </ul>
+            </div>
+          </nav>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App
